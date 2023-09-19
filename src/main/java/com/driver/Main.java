b6466c4490d1a8f022dd25a82e8063c0dd4ca509
joydeepsarkar99 <jdsarkar99@gmail.com>
2023-09-19 12:14:41 +0530

    first attempt submission

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        //we cant directly access the private instance variable of the RWOnly class
+        //obj.name = "Joydeep";
+        //System.out.println(obj.name);
+
+        // after using getter and setter
+        obj.setName("Joydeep");
+        String s = obj.getName();
+        System.out.println(s);
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public void setName(String name){  //setters
+        this.name = name;
+    }
+    public String getName(){ //getters
+        return name;
+    }
 }
